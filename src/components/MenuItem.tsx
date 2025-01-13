9b4d990f1b9050b868e25c3e90d0a41e64b8f597
Alex Young <alex.young@modlify.io>
2025-01-13 16:38:02 +0000

    feat: 🎸 menu cta style

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -34,8 +34,6 @@ export const MenuItem: FC<Props> = ({ item, isActive }) => {
             "text-sm",
             "px-5",
             "py-2.5",
-            "mr-2",
-            "mb-2",
             "dark:bg-blue-600",
             "dark:hover:bg-blue-700",
             "focus:outline-none",
