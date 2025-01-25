10a1d7765268ca3938f0448ee004329eb8413b9f
Alex Young <alex.young@modlify.io>
2025-01-25 19:22:17 +0000

    feat: 🎸 richer new preview

--- src/collections/news.ts
+++ src/collections/news.ts
@@ -19,6 +19,19 @@ const newsSchema = z.object({
   pages: z.array(
     z.object({ id: z.string(), slug: z.string(), title: z.string() }),
   ),
+  author: z.object({
+    id: z.string(),
+    name: z.string(),
+    photo: z
+      .object({
+        url: z.string(),
+        title: z.string(),
+        width: z.number(),
+        height: z.number(),
+      })
+      .optional(),
+    slug: z.string(),
+  }),
 });
 
 export type News = z.TypeOf<typeof newsSchema>;
@@ -29,22 +42,50 @@ export const news = defineCollection({
       content_type: "news",
     });
 
-    return response.items.map((item) => ({
-      id: item.sys.id,
-      title: item.fields.title,
-      content: item.fields.content,
-      summary: item.fields.summary,
-      when: new Date(item.sys.createdAt),
-      pages:
-        item.fields.pages?.map((page) => {
-          if ("fields" in page)
-            return {
-              id: page.sys.id,
-              slug: `/${slugup(page)}`,
-              title: page.fields.title,
-            };
-        }) ?? [],
-    }));
+    return response.items.map((item) => {
+      const author =
+        item.fields.author && "fields" in item.fields.author
+          ? {
+              id: item.fields.author.sys.id,
+              name: item.fields.author.fields.name,
+              slug: item.fields.author.fields.slug,
+              photo:
+                item.fields.author.fields.photo &&
+                "fields" in item.fields.author.fields.photo &&
+                item.fields.author.fields.photo.fields.file &&
+                "details" in item.fields.author.fields.photo.fields.file &&
+                item.fields.author.fields.photo.fields.file.details.image
+                  ? {
+                      url: item.fields.author.fields.photo.fields.file.url,
+                      title: item.fields.author.fields.photo.fields.title,
+                      width:
+                        item.fields.author.fields.photo.fields.file.details
+                          .image.width,
+                      height:
+                        item.fields.author.fields.photo.fields.file.details
+                          .image.height,
+                    }
+                  : undefined,
+            }
+          : undefined;
+      return {
+        id: item.sys.id,
+        title: item.fields.title,
+        content: item.fields.content,
+        summary: item.fields.summary,
+        when: new Date(item.sys.createdAt),
+        pages:
+          item.fields.pages?.map((page) => {
+            if ("fields" in page)
+              return {
+                id: page.sys.id,
+                slug: `/${slugup(page)}`,
+                title: page.fields.title,
+              };
+          }) ?? [],
+        author,
+      };
+    });
   },
   schema: newsSchema,
 });
