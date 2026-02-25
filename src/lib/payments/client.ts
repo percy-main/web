39462a33588b01a944c9fa2d4debe8fe7aa475ff
Alex Young <alex.young@percymain.org>
2026-02-25 08:20:49 +0000

    debug: log stripe secret key prefix at runtime

--- src/lib/payments/client.ts
+++ src/lib/payments/client.ts
@@ -1,4 +1,6 @@
 import { STRIPE_SECRET_KEY } from "astro:env/server";
 import { Stripe } from "stripe";
 
+console.log("[payments/client] STRIPE_SECRET_KEY prefix:", STRIPE_SECRET_KEY?.substring(0, 12));
+
 export const stripe = new Stripe(STRIPE_SECRET_KEY);
